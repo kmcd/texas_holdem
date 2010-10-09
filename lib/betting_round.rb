b65795410f0020fec32caedd16a38e2c6ff73c11
Keith McDonnell <keith@dancingtext.com>
2010-10-09 17:18:48 +0100

    Betting > minimum raise and everyone matching OK

--- lib/betting_round.rb
+++ lib/betting_round.rb
@@ -1,37 +1,64 @@
 class BettingRound
-  attr_reader :pot
+  attr_reader :players
   
-  def initialize(players,hand)
-    @players, @hand = players, hand
-    @betting_circle = players.enum_for
-    @pot = 0
-    @bets = []
+  def initialize(players,hand, minimum_bet)
+    @players, @hand, @minimum_bet = players, hand, minimum_bet
+    @bets, @betting_circle = [], @players.enum_for
   end
   
   def next_player
-    @betting_circle.next
+    @current_player = @betting_circle.next
     
     rescue StopIteration
       @betting_circle.rewind
       @betting_circle.next
   end
   
-  def check(player)
-    @bets << :check
+  def check
+    @bets << nil
+    next_player
   end
   
-  def raise(player, amount)
-    @bets << :raise
-    @pot += amount
+  def raise(amount)
+    return if amount < @minimum_bet
+    @bets << amount
+    next_player
   end
   
-  def fold(player)
-    @players.delete player
-    @hand.fold player # TODO: use an Observer instead?
+  def pot
+    @bets.compact.reduce :+
+  end
+  
+  def fold
+    @players.delete current_player
+    @hand.fold current_player # TODO: use an Observer instead?
+    next_player
   end
   
   def finished?
-    return true if @players.size == 1
-    @bets[-@players.size..-1].all? {|bet| bet == :check }
+    return true if only_one_player_remaining?
+    all_remaining_bets_checked? || everyone_bet_equal_amount?
+  end
+  
+  def current_player
+    @current_player || next_player
+  end
+  
+  private
+  
+  def everyone_bet_equal_amount?
+    remaining_players_last_bets.uniq.size == 1
+  end
+  
+  def all_remaining_bets_checked?
+    remaining_players_last_bets.all? {|bet| bet.nil? }
+  end
+  
+  def remaining_players_last_bets
+    @bets[-@players.size..-1]
+  end
+  
+  def only_one_player_remaining?
+    @players.size == 1
   end
 end
--- test/test_betting_round.rb
+++ test/test_betting_round.rb
@@ -5,7 +5,8 @@ class BettingRoundTest < Test::Unit::TestCase
   
   def setup
     super
-    @betting_round = BettingRound.new [@slim, @scotty, @doyle], @hand
+    @minimum_bet = 2.5
+    @betting_round = BettingRound.new [@slim, @scotty, @doyle], @hand, @minimum_bet
     @hand.deal
   end                                    
                              
@@ -13,41 +14,59 @@ class BettingRoundTest < Test::Unit::TestCase
     assert_equal @slim, @betting_round.next_player
   end                            
   
-  test "betting continues clockwise around the table" do
+  test "play continues clockwise around the table" do
     assert_equal @slim, @betting_round.next_player
     assert_equal @scotty, @betting_round.next_player
     assert_equal @doyle, @betting_round.next_player 
     assert_equal @slim, @betting_round.next_player
-  end                      
+  end
+  
+  test "betting continues clockwise around the table" do
+    assert_equal @slim, @betting_round.current_player
+    @betting_round.raise 20
+    assert_equal @scotty, @betting_round.current_player
+    @betting_round.raise 20
+    assert_equal @doyle, @betting_round.current_player
+  end
   
   test "should add raise to the pot" do
-    @betting_round.raise @betting_round.next_player, 20
+    @betting_round.raise 20
     assert_equal 20, @betting_round.pot
   end
                               
   test "should finish if all players check" do
-    @betting_round.check @betting_round.next_player
-    @betting_round.check @betting_round.next_player
-    @betting_round.check @betting_round.next_player
+    @betting_round.check
+    @betting_round.check 
+    @betting_round.check
     assert @betting_round.finished?
   end
   
   test "should finish if all players check or fold" do
-    @betting_round.check @betting_round.next_player
-    @betting_round.check @betting_round.next_player
-    @betting_round.fold @betting_round.next_player
+    @betting_round.check 
+    @betting_round.check
+    @betting_round.fold
     assert @betting_round.finished?
   end
   
   test "should remove player from hand if they fold" do
-    player = @betting_round.next_player
-    @betting_round.fold player
+    player = @betting_round.current_player
+    @betting_round.fold
     assert_nil @hand.players.find {|p| p == player }
   end
   
-  test "should finish when all fold" do
-    @betting_round.fold @betting_round.next_player
-    @betting_round.fold @betting_round.next_player
+  test "should finish when all but one fold" do
+    @betting_round.fold
+    @betting_round.fold
     assert @betting_round.finished?
   end
-end                                 
+  
+  test "should have minimum raise equal to big blind" do
+    assert_block { @betting_round.raise(@minimum_bet) }
+    assert_nil @betting_round.raise(@minimum_bet - 1)
+  end
+  
+  test "betting finishes when everyone has put in equal amount" do
+    @betting_round.players.size.times { @betting_round.raise 20 }
+    assert @betting_round.finished?
+  end
+end
